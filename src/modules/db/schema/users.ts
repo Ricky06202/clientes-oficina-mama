217fa7d0e593e11cbe1178451aaa1fec25d1a67a
Ricardo A. Sanjur G. <ricardosanjurg@gmail.com>
2024-11-28 17:45:26 -0500

    logramos hacerlo funcionar

--- src/modules/db/schema/users.ts
+++ src/modules/db/schema/users.ts
@@ -1,7 +1,7 @@
-import { int, mysqlTable, serial, varchar } from 'drizzle-orm/mysql-core'
+import { int, mysqlTable, varchar } from 'drizzle-orm/mysql-core'
 
 export const users = mysqlTable('users_table', {
-	id: serial().primaryKey(),
+	id: int().primaryKey().autoincrement(),
 	name: varchar({ length: 255 }).notNull(),
 	age: int().notNull(),
 	email: varchar({ length: 255 }).notNull().unique(),
